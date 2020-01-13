729629b19f09a7ff63be73688e37f0ebeffae663
joy <chengfg@bc.edu>
2020-01-13 18:38:32 -0500

    message

---
+++ new/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  new
+//
+//  Created by cheng jiayi on 2020/1/13.
+//  Copyright © 2020 cheng jiayi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //kooo
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let a=10
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
